c6edbb82b43d802fac3518ede48467940fa820b3
developerTamanna <astamanna57@gmail.com>
2025-09-08 14:43:29 +0600

    file structure and build simple layout

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Banner from "./components/Banner/Banner";
+
+export default function Home() {
+  return (
+
+    <section>
+      <div>
+        <Banner></Banner>
+      </div>
+   </section>
+  );
+}
