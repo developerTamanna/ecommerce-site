ad3a73a4cbde3896692c01f661426fa9ffb14ef9
developerTamanna <astamanna57@gmail.com>
2025-09-08 14:45:56 +0600

    banner implement

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
-// const Banner = () => {
-//   return (
-//     <section>
-//       <div>
-//         <h1 className='text-2xl text-green-500'>banner</h1>
-//       </div>
-//     </section>
-//   );
-// };
+const Banner = () => {
+  return (
+    <section>
+      <div>
+        <h1 className='text-2xl text-green-500'>banner</h1>
+      </div>
+    </section>
+  );
+};
 
-// export default Banner;
+export default Banner;
